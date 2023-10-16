2b631fc178d20c413802d0a8e17c4f4c8302f1d9
ilya <ilya.vas@outlook.com>
2023-10-16 13:03:33 +0300

    user profile views added , user model upgrd

--- PTU16_technoforge/main/models.py
+++ PTU16_technoforge/main/models.py
@@ -6,9 +6,18 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class UserForge(AbstractUser):
+    GENDER = (
+        ('Male', _('Male')),
+        ('Female', _('Female')),
+    )
+
+      
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=20)
-    avatar = models.ImageField(upload_to='media/avatars/')
+    avatar = models.ImageField(upload_to='media/avatars/', null=True, blank=True) 
+    bio = models.TextField( blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='user_forge_set',
@@ -21,3 +30,4 @@ class UserForge(AbstractUser):
         blank=True,
         verbose_name='user permissions',
     )
+    gender = models.CharField(max_length=10, choices=GENDER, default='Male')
--- PTU16_technoforge/main/views.py
+++ PTU16_technoforge/main/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView
+from .models import UserForge
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+class UserProfileDetailView(DetailView):
+    """
+    View for displaying the user profile details.
+    """
+    model = UserForge
+    template_name = 'technoforge/user_profile.html'
+
+class UserProfileUpdateView(UpdateView):
+    """
+    View for updating the user profile details.
+    """
+    model = UserForge
+    template_name = 'technoforge/edit_user_profile.html'
+    fields = ['phone', 'avatar', 'bio', 'birthday', 'github', 'gender']
+    success_url = reverse_lazy('user_profile')  
+
+@login_required
+def user_profile(request):
+    """
+    View for displaying the user profile of the currently logged-in user.
+    """
+    # Get the profile of the current user
+    user_profile = UserForge.objects.get(username=request.user.username)
+    return render(request, 'technoforge/user_profile.html', {'user_profile': user_profile})
+
+def view_user_profile(request, username):
+    """
+    View for displaying the user profile of a specific user.
+    """
+    # Get the profile of the user by their username
+    user_profile = get_object_or_404(UserForge, username=username)
+    return render(request, 'technoforge/view_user_profile.html', {'user_profile': user_profile})
